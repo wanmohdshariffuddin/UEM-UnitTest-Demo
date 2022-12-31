9f4b17add3df6c5942b7c3006402a5a2ce942b0f
Bwan109 <brandon@virtuosity-global.com>
2022-12-31 21:02:27 +0800

    added diff unit tests

--- client/src/test/App_Unit.test.js
+++ client/src/test/App_Unit.test.js
@@ -14,16 +14,26 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { render, screen } from '@testing-library/react'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it("renders without crashing", () => {
+test("renders without crashing", () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
 
-// To test is there this h1 title named Hello World React
+describe('About', () => {
+  test('About renders correctly', () => {
+    render( <App /> )
+    expect(screen.getByText("Votes:")).toBeInTheDocument()
+  })
+
+})
+
+
+//To test is there this h1 title named Hello World React
 describe('Test App Entry point', function () {
   it('should have a h1 tag with Hello world React!', function () {
     const wrapper = shallow(<App />);
@@ -32,7 +42,7 @@ describe('Test App Entry point', function () {
  });
 
 
- it("renders Nav Title", () => {
+ test("renders Nav Title", () => {
   const wrapper = shallow(<App />);
   const welcome = <strong className="navbar-item">Movie Voting App 2022</strong>;
   expect(wrapper.contains(welcome)).toEqual(true);
