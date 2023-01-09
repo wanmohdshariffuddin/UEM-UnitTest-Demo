7c2a1e2ce3762e73e23229b827a181a904cc2224
Bwan109 <brandon@virtuosity-global.com>
2023-01-09 15:46:29 +0800

    Added Integration test to yml file

--- client/src/tests/Integration.spec.js
+++ client/src/tests/Integration.spec.js
@@ -17,12 +17,12 @@ test('Verify the interface link between the votes number component and vote butt
     // locate the movie 3 vote button with xPath
     const voteButton = page.locator('//*[@id="root"]/div/section/div/div/div[3]/div/div[3]/div/button')
     // then click the button with left click and click it twice
-    await voteButton.click({button: "left", clickCount: 2});
+    await voteButton.click({button: "left", clickCount: 1});
 
     // locate the movie 3 vote numbers element with xPath
     const voteNumDisplay = page.locator('//*[@id="root"]/div/section/div/div/div[3]/div/div[2]/div')
     // then check is the value of the element same after we click
-    await expect(voteNumDisplay).toHaveText("Votes: 9")
+    await expect(voteNumDisplay).toHaveText("Votes: 1")
 
 });
 
