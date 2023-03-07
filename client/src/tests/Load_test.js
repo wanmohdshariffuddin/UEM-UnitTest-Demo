64c48fd54b765fea66b3acb5015ff5619e62c7bc
virtuosity-dev <brandon@virtuosity-global.com>
2023-03-07 21:30:26 +0800

    Changed Load_test.js

--- client/src/tests/Load_test.js
+++ client/src/tests/Load_test.js
@@ -15,12 +15,12 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  insecureSkipTLSVerify: true,
+  insecureSkipTLSVerify: true,  //since running locally so skip TLS verify
   noConnectReuse: false,
   stages: [
-    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes.
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
+    { duration: '1m', target: 50 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '1m', target: 50 }, // stay at 100 users for 10 minutes.
+    { duration: '1m', target: 0 }, // ramp-down to 0 users
   ], 
   threshold: {
     http_req_duration: ['p(99)<150'], // 99% of requests must complete below 150ms
