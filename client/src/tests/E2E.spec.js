5d2b8d0468f8036eb8ecec676f1c89c082e9e2bc
Wan Mohd Shariffuddin Bin Wan Mohd Ariff - UEM Edgenta <shariffuddin@edgenta.com>
2023-03-14 12:40:15 +0800

    added playwright test

--- client/src/tests/E2E.spec.js
+++ client/src/tests/E2E.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 
 test('Verify the website link loads smoothly and displays the home page title', async ({ page }) => {
-    await page.goto('http://localhost:3001/');
+    await page.goto('http://localhost:8081/');
   
     const locator = page.locator('//*[@id="root"]/div/nav/section/div/strong')
     await expect(locator).toHaveText("Movie Voting App 2022");
@@ -13,10 +13,11 @@ test('Verify the website link loads smoothly and displays the home page title', 
 
 test('Verify the interface link between the votes number component and vote button. i.e. clicking the  movie 3 vote button should increase the votes numbers on the UI', async ({ page }) => {
 
-    await page.goto('http://localhost:3001/');
+    await page.goto('http://localhost:8081/');
 
     // locate the movie 3 vote button with xPath
     const voteButton = page.locator('//*[@id="root"]/div/section/div/div/div[3]/div/div[3]/div/button')
+	                               
     // then click the button with left click and click it twice
     await voteButton.click({button: "left", clickCount: 1});
 
